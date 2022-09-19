75fa809f9399ea06c72c47802494e8a29a14d61c
MisterMagik <denis.panek2004@op.pl>
2022-09-19 22:22:24 +0200

    Dodano uchwyt do db

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php 
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "root";
+    $databasename = "samp";
+
+?>
--- index.php
+++ index.php
@@ -1,11 +1,20 @@
+<?php
+    require_once("connection.php");
+?>
+
 <!DOCTYPE html>
 <html >
 <head>
     <meta charset = "UTF-8">
-    <title> Samp-Rp </title>
+    <title> samp-rp </title>
+    <link rel="stylesheet" href="styles/style.css">
+
     
 </head>
 <body>
+    <div class = "container">
+        <p> Tutaj bedzie strona naszego gamemod'a </p>
 
+    </div>
 
 </body>
